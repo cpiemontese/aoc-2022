14c3b5e7a914a924440db3fdaeaae372324de065
Cristiano Piemontese <cristiano.piemontese@prima.it>
2022-12-01 16:07:27 +0100

    setup day1 & remove useless code

--- lib/aoc2022.ex
+++ lib/aoc2022.ex
@@ -1,18 +1,3 @@
 defmodule Aoc2022 do
-  @moduledoc """
-  Documentation for `Aoc2022`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Aoc2022.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
+  @moduledoc false
 end
---
+++ lib/day1/day1.ex
@@ -0,0 +1,2 @@
+defmodule Aoc2022.Day1 do
+end
