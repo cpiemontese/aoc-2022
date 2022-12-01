5dcc5200db47c0851180cffcdc8cdd2feeff665c
Cristiano Piemontese <cristiano.piemontese@prima.it>
2022-12-01 17:22:27 +0100

    add failing test

--- lib/day1/day1.ex
+++ lib/day1/day1.ex
@@ -1,2 +1,5 @@
 defmodule Aoc2022.Day1 do
+  def run(input_file) do
+    input_file
+  end
 end
--- test/aoc2022_test.exs
+++ test/aoc2022_test.exs
@@ -1,3 +1,29 @@
 defmodule Aoc2022Test do
   use ExUnit.Case
+
+  test "day1 is able to compute the most calories carried by an elf" do
+    calories = "1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000"
+
+    tmp_file_path = "./test/tmp_file.txt"
+    file = File.open!(tmp_file_path, [:write])
+    on_exit(fn -> File.rm!(tmp_file_path) end)
+
+    File.write!(tmp_file_path, calories)
+
+    result = Aoc2022.Day1.run(tmp_file_path)
+    assert result == 24_000
+  end
 end
