1443dc4dd519fa64443b50ce6d17c35e0ce3c85e
Cristiano Piemontese <cristiano.piemontese@prima.it>
2022-12-01 17:34:10 +0100

    make test pass

--- lib/day1/day1.ex
+++ lib/day1/day1.ex
@@ -1,5 +1,19 @@
 defmodule Aoc2022.Day1 do
   def run(input_file) do
-    input_file
+    stream = File.stream!(input_file)
+
+    {_, max} =
+      Enum.reduce(stream, {0, -1}, fn element, {sum, current_max} ->
+        case element do
+          "\n" ->
+            {0, max(sum, current_max)}
+
+          _ ->
+            number = element |> String.slice(0..-2) |> String.to_integer()
+            {sum + number, current_max}
+        end
+      end)
+
+    max
   end
 end
